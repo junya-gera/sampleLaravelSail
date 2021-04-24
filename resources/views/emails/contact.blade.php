fee2391f75636b7a35012fc453c5d0790d1a1e93
Junya WADA <junya-wada@msen.jp>
2021-04-24 12:33:41 +0900

    supervisor の設定ファイルを追加

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,4 +1,5 @@
 <body>
     <h1>お問い合わせ</h1>
     <p>お問い合わせです</p>
+    <p>supervisor 設定後</p>
 </body>
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactMailController;
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
 Route::get('contact', [ContactMailController::class, 'contact']);
